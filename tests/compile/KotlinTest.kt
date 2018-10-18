ce66e0325e64839cbf49970afefae83a702861b9
Warren MacEvoy <wmacevoy@gmail.com>
2018-10-17 22:50:45 -0600

    runtime versions

--- tests/compile/KotlinTest.kt
+++ tests/compile/KotlinTest.kt
@@ -1,6 +1,3 @@
-package test.me3
-
 fun main(args : Array<String>) {
-  // prints null but no runtime info on kotlin...
-  println("v" + System.getProperty("kotlin.version"))
+    println("v" + KotlinVersion.CURRENT);
 }
