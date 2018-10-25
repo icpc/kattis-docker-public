191d27804e5274105189879c923db255db40e496
Warren MacEvoy <wmacevoy@gmail.com>
2018-10-25 14:28:24 -0600

    newline

--- tests/compile/gcc-test.c
+++ tests/compile/gcc-test.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-  printf("v%d.%d.%d",__GNUC__,__GNUC_MINOR__,__GNUC_PATCHLEVEL__);
+  printf("v%d.%d.%d\n",__GNUC__,__GNUC_MINOR__,__GNUC_PATCHLEVEL__);
   return 0;
 }
