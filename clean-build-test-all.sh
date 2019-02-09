3a10bec4031c8a6922b4804d7c7f1e8829115373
Warren MacEvoy <wmacevoy@gmail.com>
2019-02-08 23:24:03 -0700

    clean-build-test-all.sh osx

--- clean-build-test-all.sh
+++ clean-build-test-all.sh
@@ -4,6 +4,10 @@
 # clean-build-test-all.sh [--branch="master"] [--tmp=$(mktemp -d)] [--keep=false] [--help]
 #
 
+pushd $(dirname "${BASH_SOURCE[0]}") >/dev/null
+proj_dir="$PWD"
+popd >/dev/null
+
 tmp=""
 branch="master"
 keep="false"
@@ -26,10 +30,12 @@ done
 
 if [ "$tmp" = "" ]
 then
-    TMP_DIR=$(mktemp -d)
+    TAG=$(dd bs=1024 count=1 if=/dev/urandom of=/dev/stdout 2>/dev/null | openssl sha256 | tail -1 | sed -e 's/(stdin)= //')
+    TMP_DIR="./tmp.$TAG"
+    mkdir -p "$TMP_DIR"
 else
     TMP_DIR="$tmp"
-    mkdir -p "$TMP_DIR"    
+    mkdir -p "$TMP_DIR"
 fi
 
 function cleanup {
