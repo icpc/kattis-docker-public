2f3afe721c9c0ccacb1af181bdadf78f01cab744
Warren MacEvoy <wmacevoy@gmail.com>
2019-02-09 03:31:20 -0500

    git save log

--- clean-build-test-all.sh
+++ clean-build-test-all.sh
@@ -11,6 +11,8 @@ popd >/dev/null
 tmp=""
 branch="master"
 keep="false"
+ssh=true
+https=false
 
 while [ $# -gt 0 ]
 do
@@ -20,6 +22,8 @@ do
 	--tmp=*)    tmp=${arg#--tmp=};;
 	--branch=*) branch=${arg#--branch=};;
 	--keep=*)   keep=${arg#--keep=};;
+	--ssh)      ssh=true; https=false;;
+	--https)    ssh=false; https=true;;	
 	*)
 	    echo "usage $0 [--branch=master] [--tmp=$(mktemp -d)] [--keep=false] [--help]"
 	    exit 1
@@ -31,7 +35,7 @@ done
 if [ "$tmp" = "" ]
 then
     TAG=$(dd bs=1024 count=1 if=/dev/urandom of=/dev/stdout 2>/dev/null | openssl sha256 | tail -1 | sed -e 's/(stdin)= //')
-    TMP_DIR="./tmp.$TAG"
+    TMP_DIR="$proj_dir/tmp.$TAG"
     mkdir -p "$TMP_DIR"
 else
     TMP_DIR="$tmp"
@@ -39,37 +43,67 @@ else
 fi
 
 function cleanup {
-    /bin/rm -rf "$TMP_DIR"
+    if [ -f "$TMP_DIR/clean-build-test-all.log" -a -f "$TMP_DIR/kattis-docker/.git" ]
+    then
+	echo "saving log to test branch."
+	cd "$TMP_DIR/kattis-docker"
+	if ! git branch -a | egrep '^ *(remotes/origin/)?test$'
+	then
+	    git branch test
+	fi
+	git checkout test
+	cp ../clean-build-test-all.log .
+	git add clean-build-test-all.log
+	git commit -m "test"
+	git push --set-upstream origin test
+    fi
+    
+    if [ "$keep" != "true" ]
+    then
+	/bin/rm -rf "$TMP_DIR"
+    else
+	echo "keeping $TMP_DIR"
+    fi
 }
 
-if [ "$keep" != "true" ]
+trap cleanup EXIT
+
+cd "$TMP_DIR"
+
+uname -a | tee -a clean-build-test-all.log
+echo "TMP_DIR=$TMP_DIR" 2>&1 | tee -a clean-build-test-all.log
+
+if [ "$ssh" = "true" ]
 then
-    trap cleanup EXIT
+    git_address=git@github.com:icpc/kattis-docker.git
+else
+    git_address=https://github.com/icpc/kattis-docker
 fi
 
-cd  "$TMP_DIR"
+echo ./clean-build-test-all.sh --branch="$branch" --keep="$keep" --tmp="$tmp"  2>&1 | tee -a clean-build-test-all.log
 
-echo git clone --single-branch --branch "$branch" https://github.com/icpc/kattis-docker . 2>&1 | tee -a clean-build-test-all.log
+echo git clone --single-branch --branch "$branch" $git_address 2>&1 | tee -a clean-build-test-all.log
 
-if ! git clone --single-branch --branch "$branch" https://github.com/icpc/kattis-docker . 2>&1 | tee -a clean-build-test-all.log
+if ! git clone --single-branch --branch "$branch" $git_address 2>&1 | tee -a clean-build-test-all.log
 then
     echo "clone errors."
-    exit 1
 fi
 
-echo bin/setup --build 2>&1 | tee -a clean-build-test-all.log
+echo kattis-docker/bin/setup --build 2>&1 | tee -a clean-build-test-all.log
 
-if ! bin/setup --build 2>&1 | tee -a clean-build-test-all.log
+if ! kattis-docker/bin/setup --build 2>&1 | tee -a clean-build-test-all.log
 then
     echo "setup errors." 2>&1 | tee -a clean-build-test-all.log
     exit 1
 fi
 
-echo . context >>clean-build-test-all.log 2>&1 2>&1 | tee -a clean-build-test-all.log
+echo . kattis-docker/context 2>&1 | tee -a clean-build-test-all.log
 
-. context
+. kattis-docker/context
 
-if ! tests/all >>clean-build-test-all.log 2>&1 | tee -a clean-build-test-all.log
+echo kattis-docker/tests/all 2>&1 | tee -a clean-build-test-all.log
+
+if ! kattis-docker/tests/all 2>&1 | tee -a clean-build-test-all.log
 then
     echo "test errors." 2>&1 | tee -a clean-build-test-all.log
     exit 1
